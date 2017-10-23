db4e90e233f1c1f60c08308f9fa31c2c2b8743b0
Jin Choi <jinchoi721@gmail.com>
2017-10-22 22:11:03 -0400

    update

--- make_command_public_100.1.py
+++ make_command_public_100.1.py
@@ -6,6 +6,6 @@ import sys
 
 for line in open(sys.argv[1],'r'):
     spl = line.strip().split('\t')
-    auth = sys.argv[2]
+
     print "curl -o "+spl[1]+".fastq -X GET http://api.metagenomics.anl.gov/1/download/"+spl[1]+"?file=100.1"
 
